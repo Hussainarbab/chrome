d3e9de2e9ec7bf7509d206e93493a212a7ff6e4b
Hussainarbab <arbabhussan63@gmail.com>
2024-09-05 04:09:05 -0700

    first

---
+++ app.js
@@ -0,0 +1,13 @@
+   
+    const ok =  document.querySelector("body");
+    const okk =  document.querySelector("body");
+
+function dark(){
+ok.style.backgroundColor= "black"
+}
+
+
+
+function light(){
+    okk.style.backgroundColor= "white"
+    }
